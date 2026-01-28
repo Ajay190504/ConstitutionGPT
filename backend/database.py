e9bf5a44906dac12b9ec717d9d0ff795a7b5986c
DevashreeA <devashreea14@gmail.com>
2026-01-28 18:37:20 +0530

    mongodb url changed

--- backend/database.py
+++ backend/database.py
@@ -8,7 +8,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-mongo_uri = os.getenv("MONGODB_URI", "mongodb://localhost:27017/")
+mongo_uri = os.getenv("MONGODB_URI")
+
 
 client = MongoClient(mongo_uri)
 db = client["constitution_gpt"]
